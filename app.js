35732c540ad2d3f45b0ae0acbe81d7a043842a6b
constance yu <faithwyu@gmail.com>
2019-07-25 15:53:25 -0700

    dynamic db, new path

--- app.js
+++ app.js
@@ -66,72 +66,99 @@ const server = http.createServer((req, res) => {
     res.end('Hello World\n');
 });
 
-/*
-    if the directory contains header.md or footer.md,
-    it is very likely that they do not have a commonly defined title such as '# TITLE'
-*/
-const parseMarkdown = text => {
-    const match_res = text.match(/^# (.*)\n/m);
-    if (match_res) {
-        // since i am using () to group the regex, it will be stored as
-        // ['# TITLE', 'TITLE] and we are interested in the ele in pos 1
-        return match_res[1];
+const base_url =  `http://localhost:3000/`;
+
+const revision = require('child_process')
+.execSync('git rev-parse HEAD')
+.toString().trim()
+
+let existing_table_names = []
+let current_columns = []
+
+const createDefaultTable = (table_name) => {
+    const create_table_query = `CREATE TABLE IF NOT EXISTS ${table_name} (
+        path        text    PRIMARY KEY
+    );`
+    console.log(`Preparing to execute table default creation query ${create_table_query}`)
+    client.query(create_table_query)
+        .catch(err => console.log(err))
+    existing_table_names.push(table_name)
+    current_columns.push('path')
+}
+
+const updateColumns = (table_name, key) => {
+    const update_column_query = `ALTER TABLE ${table_name}
+        ADD COLUMN IF NOT EXISTS ${key} text;`
+    console.log(`Preparing to execute column insertion query ${update_column_query}`)
+    client.query(update_column_query)
+        .catch(err => console.log(err))
+    current_columns.push(key)
+}
+
+const execQuery = (table_name, file_path, file_entries) => {
+    Object.keys(file_entries).map(key => {
+        if (!current_columns.includes(key))
+            updateColumns(table_name, key)
+    })
+    const query_schema = current_columns.join(', ')
+    let current_values = []
+    for (let column of current_columns) {
+        console.log('column', column)
+        if (column === 'path') {
+            current_values.push(file_path)
+        } else {
+            current_values.push(file_entries[column] ? file_entries[column] : 'NULL')
+        }
     }
-};
+    const value_field = current_values .join('\', \'')
+    const insert_data_query = `INSERT INTO ${table_name} (${query_schema}) VALUES ('${value_field}');`;
+    console.log(`Preparing to execute data insertion query ${insert_data_query}`)
+    client.query(insert_data_query)
+        .catch(err => {
+            console.log(`Error executing database query '${insert_data_query}': `, err)
+        })
+}
+
+const traverseTree = () => octokit.git.getTree({
+    owner: owner,
+    repo: repo,
+    tree_sha: revision,
+    recursive: 1,
+}).then(response => {
+    if (path == '')
+        return response.data.tree.filter(obj => obj.type === 'blob' && !obj.path.startsWith('.github') && obj.path.endsWith('.md'))
+    else
+        // error prone if the directory path has the exact string bc using 'includes'
+        // e.g. if i only want hackathons/hack, this method gives me hackathons/ too
+        return response.data.tree.filter(obj => obj.type === 'blob' && !obj.path.startsWith('.github') && obj.path.endsWith('.md') && obj.path.includes(path))
+}).then(files => 
+    files.map(file => {
+        const wrapper = {}
+        const idx_html = file.path.replace('.md', '.idx.html')
+        wrapper[base_url.concat(idx_html)] = `/${file.path}`
+        return wrapper
+    })
+).then(urls => urls.map((url_object) => {
+    for (const [url, path] of Object.entries(url_object)) {
+        request({uri: url, json: true})
+        .then(content => {
+            console.log('the request url is: ', url)
+            console.log('the title and description of this url is: ', content.tables[0].entries)
+            console.log('the entire content block looks like: ', content)
+
+            content.tables.map(table => {
+                const table_name = table.name
+                if (!existing_table_names.includes(table_name)) {
+                    createDefaultTable(table_name)
+                }
+                execQuery(table_name, path, table.entries)
+            })
+        })
+    }
+}))
 
 server.listen(server_port, hostname, () => {
     console.log(`Server running at http://${hostname}:${server_port}/`);
-
-    const base_url =  `http://localhost:3000/`;
-
-    const revision = require('child_process')
-    .execSync('git rev-parse HEAD')
-    .toString().trim()
-
-    const execQuery = (table_name, file_path, file_title) => {
-        const create_table_query = `CREATE TABLE IF NOT EXISTS ${table_name} (
-            path     text    PRIMARY KEY,
-            title    text
-        );`;
-        client.query(create_table_query)
-            .catch(err => console.log(err))
-
-        const insert_data_query = `INSERT INTO ${table_name} (path, title) VALUES ('${file_path}', '${file_title}');`;
-        console.log(`Preparing to execute data insertion query ${insert_data_query}`)
-        client.query(insert_data_query)
-            .catch(err => {
-                console.log(`Error executing database query '${insert_data_query}': `, err)
-            })
-    }
-
-    const traverseTree = () => octokit.git.getTree({
-        owner: owner,
-        repo: repo,
-        tree_sha: revision,
-        recursive: 1,
-    }).then(response => {
-        if (path == '')
-            return response.data.tree.filter(obj => obj.type === 'blob' && !obj.path.startsWith('.github') && obj.path.endsWith('.md'))
-        else
-            return response.data.tree.filter(obj => obj.type === 'blob' && !obj.path.startsWith('.github') && obj.path.endsWith('.md') && obj.path.includes(path))
-    }).then(files => 
-        files.map(file => {
-            const wrapper = {}
-            const idx_html = file.path.replace('.md', '.idx.html')
-            wrapper[base_url.concat(idx_html)] = file.path
-            return wrapper
-        })
-    ).then(urls => urls.map((url_object) => {
-        for (const [url, path] of Object.entries(url_object)) {
-            request({uri: url, json: true})
-            .then(content => {
-                console.log('the request url is: ', url)
-                console.log('the title of this url is: ', content.tables[0].entries)
-                console.log('the entire content block looks like: ', content)
-                content.tables.map(table => execQuery(table.name, path, table.entries.title))
-            })}
-        }
-    ))
 
     client.connect(err => {
         if (err) throw err;
